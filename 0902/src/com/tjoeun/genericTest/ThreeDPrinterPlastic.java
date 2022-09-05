9200d7169013ace56dbc72c55885174c6d637f6c
ChoiHyeongRok1 <105375068+ChoiHyeongRok1@users.noreply.github.com>
2022-09-05 13:55:58 +0900

    0902

---
+++ 0902/src/com/tjoeun/genericTest/ThreeDPrinterPlastic.java
@@ -0,0 +1,17 @@
+package com.tjoeun.genericTest;
+
+public class ThreeDPrinterPlastic {
+	
+	private Plastic material;
+
+	public Plastic getMaterial() {
+		return material;
+	}
+
+	public void setMaterial(Plastic material) {
+		this.material = material;
+	}
+	
+	
+	
+}
